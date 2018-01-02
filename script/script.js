86bc0dfc5a8f8a54e4844de0413bd0c2428dcd02
Aakash Pandey <Aakash.pandey@live.com>
2018-01-03 03:35:01 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,2 +1,2 @@
 var nv = $("#nav");nv.click(()=>{$("#drop").slideToggle(300);if(nv.hasClass("navt"))nv.removeClass("navt");else nv.addClass("navt");});
-window.location.href = "http://aakashpandey.96.lt";
+window.location.href = "https://aakashpandey.github.io/404";
