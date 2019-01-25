e83a3f5a6b4a8f43079a5319ee1f86cfedaae5d9
Aakash Pandey <Aakash.pandey@live.com>
2019-01-26 02:27:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("[SKYROUTES-BETA-0.5] An innovative client side router by Aakash Pandey");
-const vn = "version-q90";
-const chg = [00,01,02]
+const vn = "version-legacy1";
+const chg = "hmmmmm";
 
 var appCash = [
     '/index.html',
