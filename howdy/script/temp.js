728defc496607f1b5ff70c4cd3d4b62c6c1097c1
Aakash Pandey <Aakash.pandey@live.com>
2023-02-16 18:19:22 +0530

    Update temp.js

--- howdy/script/temp.js
+++ howdy/script/temp.js
@@ -1,6 +1,6 @@
 var tc=["#c62828","#2E7D32","#1565C0","#FF8F00","#00838F","#D81B60","#BA68C8","#607D8B","#C0CA33"];
 
-var URL = "https://httprelay.io/mcast/";
+var URL = "https://demo.httprelay.io/mcast/";
 
 var jbt = `$('<div/>').addClass("jam").hide().fadeIn(600)`;
 var nbt = `$('<div/>').addClass("nam").hide().fadeIn(600)`;
